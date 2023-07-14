870b7f54c02ba7c83b55ab3075cf10c9941ed604
mvr659 <mvr659@usask.ca>
2023-07-14 08:23:21 -0600

    a4q3 - The function replace_in() is made using recursion as per the part-C of question 3.

--- a4q3.py
+++ a4q3.py
@@ -48,3 +48,27 @@ def copy(node_chain):
     copied_node.set_next(copy(next_node))
 
     return copied_node
+
+
+def replace_in(node_chain, target, replacement):
+    """
+    Purpose:
+        Replaces each occurrence of the target value with the replacement
+    Pre-conditions:
+        :param node_chain: a node-chain, possibly empty
+        :param target: a value that might appear in the node chain
+        :param replacement: the value to replace the target
+    Post-conditions:
+        Each occurrence of the target value in the chain is replaced with
+        the replacement value.
+    Return:
+        None
+    """
+    if node_chain is None:
+        return 0
+    else:
+        walker = node_chain
+        value = walker.get_data()
+        if value == target:
+            walker.set_data(replacement)
+        return replace_in(walker.get_next(), target, replacement)
