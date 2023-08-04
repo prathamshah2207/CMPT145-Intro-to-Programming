49ece2561342c990c81cf71ec7190605a23ba142
mvr659 <mvr659@usask.ca>
2023-08-04 07:02:32 -0600

    a5q1 - Added personal info to all the files

--- a5q1.bash
+++ a5q1.bash
@@ -1,10 +1,14 @@
+# Student Name: Pratham Shah                        Section Number: 01
+# NSID: mvr659                                      Course Number: 41442
+# Student Number: 11353450                          Instructor: Lauresa Stilling
+
 pwd
 ls -a
 mkdir mvr659_cyan
 cd mvr659_cyan
 pwd
 touch file.txt
-vrbl = "Hello World"
+vrbl="Hello World"
 echo $vrbl
 echo $vrbl > second_file.txt
 cat file.txt second_file.txt
