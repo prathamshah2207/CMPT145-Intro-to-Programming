bd63e9fa8ff35f873ad33e1a5ee895cc9d977210
mvr659 <mvr659@usask.ca>
2023-08-04 12:31:30 -0600

    a5q2 - Added second function copy() as per required functionality.

--- a5q2.py
+++ a5q2.py
@@ -2,6 +2,7 @@
 # NSID: mvr659                                      Course Number: 41442
 # Student Number: 11353450                          Instructor: Lauresa Stilling
 
+import treenode as tn
 def subst(tnode, t, r):
     """
     Substitute a target value t with a replacement value r wherever it appears in the given tree.
@@ -18,3 +19,25 @@ def subst(tnode, t, r):
             tnode.set_data(r)
         subst(tnode.get_left(), t, r)
         subst(tnode.get_right(), t, r)
+
+def copy(tnode):
+    """
+    Create an exact copy of the given tree, with completely new treenodes, but exactly the same data values, in exactly the same places.
+    :param tnode: The root treenode
+    :return: A reference to the new tree.
+    """
+
+    if tnode is None:
+        return None
+    else:
+        left = copy(tnode.get_left())
+        right = copy(tnode.get_right())
+
+        data = tnode.get_data()
+        newtree = tn.Treenode(data)
+
+        newtree.set_left(left)
+        newtree.set_right(right)
+
+        return newtree
+
