7463a69591576201711d103a6fe412227b62db28
mvr659 <mvr659@usask.ca>
2023-08-07 21:11:31 -0600

    a5q2 - Made test cases for copy() function for efficient functioning.

--- a5q2_tests.py
+++ a5q2_tests.py
@@ -2,7 +2,6 @@
 # NSID: mvr659                                      Course Number: 41442
 # Student Number: 11353450                          Instructor: Lauresa Stilling
 
-import exampletrees as et
 import treenode as tn
 import a5q2 as main
 import provided_treefunctions as check_tree
@@ -43,6 +42,38 @@ reason = 'Empty binary tree'
 main.subst(tnode, t, r)
 
 if check_tree.to_string(expected_tree) != check_tree.to_string(tnode):
+    print('Test3 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
+
+# Test Case for copy(tnode) function
+
+# First
+test_item = 'copy()'
+tnode = None
+expected_tree = None
+reason = 'Empty binary tree'
+result = main.copy(tnode)
+
+if check_tree.to_string(expected_tree) != check_tree.to_string(result):
+    print('Test1 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
+
+# Second
+test_item = 'copy()'
+tnode = tn.Treenode(3)
+expected_tree = tn.Treenode(3)
+reason = 'Empty binary tree'
+result = main.copy(tnode)
+
+if check_tree.to_string(expected_tree) != check_tree.to_string(result):
+    print('Test2 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
+
+# Third
+test_item = 'copy()'
+tnode = tn.Treenode(3, tn.Treenode(5, tn.Treenode(9, None)), tn.Treenode(10, tn.Treenode(11, None)))
+expected_tree = tn.Treenode(3, tn.Treenode(5, tn.Treenode(9, None)), tn.Treenode(10, tn.Treenode(11, None)))
+reason = 'Empty binary tree'
+result = main.copy(tnode)
+
+if check_tree.to_string(expected_tree) != check_tree.to_string(result):
     print('Test3 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
 
 print('*** testing complete ***')
