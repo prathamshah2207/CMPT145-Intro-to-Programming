e8c54cfb7136620a48dca63d2cf71533c7fb6690
mvr659 <mvr659@usask.ca>
2023-08-07 22:08:49 -0600

    a5q2 - Bug Fixes and changed some reasons

--- a5q2_tests.py
+++ a5q2_tests.py
@@ -26,7 +26,7 @@ tnode = tn.Treenode(3)
 t = 3
 r = 2
 expected_tree = tn.Treenode(2)
-reason = 'Empty binary tree'
+reason = 'Single node tree value replaced target value'
 main.subst(tnode, t, r)
 
 if check_tree.to_string(expected_tree) != check_tree.to_string(tnode):
@@ -38,7 +38,7 @@ tnode = tn.Treenode(3, tn.Treenode(5, tn.Treenode(9, None)), tn.Treenode(10, tn.
 t = 9
 r = 0
 expected_tree = tn.Treenode(3, tn.Treenode(5, tn.Treenode(0, None)), tn.Treenode(10, tn.Treenode(11, None)))
-reason = 'Empty binary tree'
+reason = 'More than one node targeted value changed'
 main.subst(tnode, t, r)
 
 if check_tree.to_string(expected_tree) != check_tree.to_string(tnode):
@@ -60,7 +60,7 @@ if check_tree.to_string(expected_tree) != check_tree.to_string(result):
 test_item = 'copy()'
 tnode = tn.Treenode(3)
 expected_tree = tn.Treenode(3)
-reason = 'Empty binary tree'
+reason = 'Tree with single node'
 result = main.copy(tnode)
 
 if check_tree.to_string(expected_tree) != check_tree.to_string(result):
@@ -70,10 +70,43 @@ if check_tree.to_string(expected_tree) != check_tree.to_string(result):
 test_item = 'copy()'
 tnode = tn.Treenode(3, tn.Treenode(5, tn.Treenode(9, None)), tn.Treenode(10, tn.Treenode(11, None)))
 expected_tree = tn.Treenode(3, tn.Treenode(5, tn.Treenode(9, None)), tn.Treenode(10, tn.Treenode(11, None)))
-reason = 'Empty binary tree'
+reason = 'Tree with more than one node'
 result = main.copy(tnode)
 
 if check_tree.to_string(expected_tree) != check_tree.to_string(result):
     print('Test3 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
+
+# Test Case for diff_sum_preorder(tnode) function
+
+# First
+test_item = 'copy()'
+tnode = None
+expected_tree = None
+reason = 'Empty binary tree'
+result = main.copy(tnode)
+
+if check_tree.to_string(expected_tree) != check_tree.to_string(result):
+    print('Test1 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
+
+# Second
+test_item = 'copy()'
+tnode = tn.Treenode(3)
+expected_tree = tn.Treenode(3)
+reason = 'Tree with single node'
+result = main.copy(tnode)
+
+if check_tree.to_string(expected_tree) != check_tree.to_string(result):
+    print('Test2 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
+
+# Third
+test_item = 'copy()'
+tnode = tn.Treenode(3, tn.Treenode(5, tn.Treenode(9, None)), tn.Treenode(10, tn.Treenode(11, None)))
+expected_tree = tn.Treenode(3, tn.Treenode(5, tn.Treenode(9, None)), tn.Treenode(10, tn.Treenode(11, None)))
+reason = 'Tree with more than one node'
+result = main.copy(tnode)
+
+if check_tree.to_string(expected_tree) != check_tree.to_string(result):
+    print('Test3 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
+
 
 print('*** testing complete ***')
