4a0bb7849241c0c29644ef1858ea27f053535dac
mvr659 <mvr659@usask.ca>
2023-08-07 22:26:42 -0600

    a5q2 - Made test cases for diff_sum_inorder() function for its accurate performance

--- a5q2_tests.py
+++ a5q2_tests.py
@@ -76,37 +76,74 @@ result = main.copy(tnode)
 if check_tree.to_string(expected_tree) != check_tree.to_string(result):
     print('Test3 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
 
-# Test Case for diff_sum_preorder(tnode) function
+# Test Case for diff_sum_preorder(tnode)
 
 # First
-test_item = 'copy()'
+test_item = 'diff_sum_preorder()'
 tnode = None
 expected_tree = None
 reason = 'Empty binary tree'
-result = main.copy(tnode)
+result = main.diff_sum_preorder(tnode)
+
+if check_tree.to_string(expected_tree) != check_tree.to_string(tnode):
+    print('Test1 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
+
+## second
+test_item = 'diff_sum_preorder()'
+tnode = tn.Treenode(3)
+expected_tree = 3
+reason = 'Tree with single node'
+result = main.diff_sum_preorder(tnode)
+
+if expected_tree != result:
+    print('Test2 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
+
+## third
+test_item = 'diff_sum_preorder()'
+tnode = tn.Treenode(5, tn.Treenode(2, tn.Treenode(1, None, None),
+                                   tn.Treenode(1, tn.Treenode(0, None, None),
+                                               tn.Treenode(1, None, None))),
+                    tn.Treenode(3, tn.Treenode(1, tn.Treenode(0, None, None),
+                                               tn.Treenode(1, None, None)),
+                                tn.Treenode(2, tn.Treenode(1, None, None),
+                                            tn.Treenode(1, tn.Treenode(0, None, None),
+                                                        tn.Treenode(1, None, None)))))
+expected_tree = 6
+reason = 'Tree with more than one node'
+result = main.diff_sum_preorder(tnode)
+if expected_tree != result:
+    print('Test3 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
+
+# Test Case for diff_sum_inorder(tnode) function
+
+# First
+test_item = 'diff_sum_inorder()'
+tnode = None
+expected_tree = None
+reason = 'Empty binary tree'
+result = main.diff_sum_inorder(tnode)
 
 if check_tree.to_string(expected_tree) != check_tree.to_string(result):
     print('Test1 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
 
 # Second
-test_item = 'copy()'
+test_item = 'diff_sum_inorder()'
 tnode = tn.Treenode(3)
-expected_tree = tn.Treenode(3)
+expected_tree = 3
 reason = 'Tree with single node'
-result = main.copy(tnode)
+result = main.diff_sum_inorder(tnode)
 
-if check_tree.to_string(expected_tree) != check_tree.to_string(result):
+if expected_tree != result:
     print('Test2 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
 
 # Third
-test_item = 'copy()'
-tnode = tn.Treenode(3, tn.Treenode(5, tn.Treenode(9, None)), tn.Treenode(10, tn.Treenode(11, None)))
-expected_tree = tn.Treenode(3, tn.Treenode(5, tn.Treenode(9, None)), tn.Treenode(10, tn.Treenode(11, None)))
+test_item = 'diff_sum_inorder()'
+tnode = tn.Treenode(1, tn.Treenode(3, tn.Treenode(5, tn.Treenode(9, None)), tn.Treenode(10, tn.Treenode(11, None))))
+expected_tree = 17
 reason = 'Tree with more than one node'
-result = main.copy(tnode)
+result = main.diff_sum_inorder(tnode)
 
-if check_tree.to_string(expected_tree) != check_tree.to_string(result):
+if expected_tree != result:
     print('Test3 failed: {}: Tree after substitution is not as expected -- {}'.format(test_item, reason))
-
 
 print('*** testing complete ***')
