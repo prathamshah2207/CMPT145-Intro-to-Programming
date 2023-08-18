31555b5024c0b5e95c7632d1f11a3c16888c443b
mvr659 <mvr659@usask.ca>
2023-08-18 15:34:21 -0600

    a6q1 - Added sixth test for the testing file, and this time tested for the default iterations.

--- a6q1_testing.py
+++ a6q1_testing.py
@@ -32,7 +32,7 @@ with open("input2_2steps.txt", "r") as temp_file:
 if lst != expected:
     print("Testing Conway() with", test, "\n\tExpected:", expected, "\n\tGot:", lst)
 else:
-    print("Test1 Success...")
+    print("Test2 Success...")
 
 # Test3
 test = "Input3.txt"
@@ -46,7 +46,7 @@ with open("input3_1steps.txt", "r") as temp_file:
 if lst != expected:
     print("Testing Conway() with", test, "\n\tExpected:", expected, "\n\tGot:", lst)
 else:
-    print("Test1 Success...")
+    print("Test3 Success...")
 
 # Test4
 test = "Input4.txt"
@@ -60,7 +60,7 @@ with open("input4_3steps.txt", "r") as temp_file:
 if lst != expected:
     print("Testing Conway() with", test, "\n\tExpected:", expected, "\n\tGot:", lst)
 else:
-    print("Test1 Success...")
+    print("Test4 Success...")
 
 # Test5
 test = "Input5.txt"
@@ -74,4 +74,20 @@ with open("input5_4steps.txt", "r") as temp_file:
 if lst != expected:
     print("Testing Conway() with", test, "\n\tExpected:", expected, "\n\tGot:", lst)
 else:
-    print("Test1 Success...")
+    print("Test5 Success...")
+
+# Test6
+test = "Input6.txt"
+expected = ['---', '-ZZ', '---']
+lst = []
+with patch("builtins.input", side_effect=["n"]):
+    Conway(test)
+with open("input6_3steps.txt", "r") as temp_file:
+    for i in temp_file:
+        lst.append(i.removesuffix('\n'))
+if lst != expected:
+    print("Testing Conway() with", test, "\n\tExpected:", expected, "\n\tGot:", lst)
+else:
+    print("Test6 Success...")
+
+print("---Testing Complete---")
