7cc96d4dd79a39ca7215068ebe7e1b35f0190896
mvr659 <mvr659@usask.ca>
2023-08-18 15:23:48 -0600

    a6q1 - Added third test for the testing file, and we are using the provided input tests in testings.

--- a6q1_testing.py
+++ a6q1_testing.py
@@ -33,3 +33,17 @@ if lst != expected:
     print("Testing Conway() with", test, "\n\tExpected:", expected, "\n\tGot:", lst)
 else:
     print("Test1 Success...")
+
+# Test3
+test = "Input3.txt"
+expected = ['-*-', '---', '-*-']
+lst = []
+with patch("builtins.input", side_effect=["n"]):
+    Conway(test, 1)
+with open("input3_1steps.txt", "r") as temp_file:
+    for i in temp_file:
+        lst.append(i.removesuffix('\n'))
+if lst != expected:
+    print("Testing Conway() with", test, "\n\tExpected:", expected, "\n\tGot:", lst)
+else:
+    print("Test1 Success...")
