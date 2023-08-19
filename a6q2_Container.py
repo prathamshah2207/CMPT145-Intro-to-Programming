57aee9e6b30216bac4953b56814df463e36dd0ba
mvr659 <mvr659@usask.ca>
2023-08-19 02:27:56 -0600

    a6q2 - Made docstrings for all the functions al the classes.

--- a6q2_Container.py
+++ a6q2_Container.py
@@ -1,18 +1,38 @@
 import node as N
 
-
 class Container:
     def __init__(self):
+        """
+        Initializes a new instance of the Container class.
+        """
         self._size = 0
         self._head = None
 
     def is_empty(self):
+        """
+        Checks if the container is empty.
+
+        Returns:
+            bool: True if the container is empty, False otherwise.
+        """
         return self._size == 0
 
     def get_size(self):
+        """
+        Gets the size of the container.
+
+        Returns:
+            int: The size of the container.
+        """
         return self._size
 
     def push_data(self, value):
+        """
+        Pushes a new element with the specified value to the top of the container.
+
+        Args:
+            value: The value of the new element.
+        """
         new_node = N.Node(value)
         if self._head is None:
             self._head = new_node
@@ -22,6 +42,12 @@ class Container:
         self._size += 1
 
     def pop_data(self):
+        """
+        Removes and returns the top element from the container.
+
+        Returns:
+            The value of the top element, or None if the container is empty.
+        """
         if self._head is None:
             return None
         data = self._head.get_data()
@@ -30,4 +56,12 @@ class Container:
         return data
 
     def peek_data(self):
+        """
+        Returns the value of the top element without removing it from the container.
+
+        Returns:
+            The value of the top element, or None if the container is empty.
+        """
+        if self._head is None:
+            return None
         return self._head.get_data()
--- a6q2_Stack.py
+++ a6q2_Stack.py
@@ -1,20 +1,46 @@
 from a6q2_Container import Container
 
-
 class Stack(Container):
     def __init__(self):
+        """
+        Initializes a new instance of the Stack class.
+        """
         super().__init__()
         self._top = None
 
     def size(self):
+        """
+        Gets the size of the stack.
+
+        Returns:
+            int: The size of the stack.
+        """
         return self.get_size()
 
     def push(self, data):
+        """
+        Pushes a new element with the specified data to the top of the stack.
+
+        Args:
+            data: The data of the new element.
+        """
         self.push_data(data)
         self._top = self._head.get_data()
 
     def pop(self):
+        """
+        Removes and returns the top element from the stack.
+
+        Returns:
+            The data of the top element, or None if the stack is empty.
+        """
         return self.pop_data()
 
     def peek(self):
+        """
+        Returns the data of the top element without removing it from the stack.
+
+        Returns:
+            The data of the top element, or None if the stack is empty.
+        """
         return self.peek_data()
